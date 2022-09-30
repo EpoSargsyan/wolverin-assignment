5db220828cc0255fc59776ad0366dc4e44e89c32
EpoSargsyan <110049829+EpoSargsyan@users.noreply.github.com>
2022-09-30 14:24:16 +0400

    Add files via upload

--- sep-29-2022/Enum.cpp
+++ sep-29-2022/Enum.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+enum class Colors {Red, Blue, Green};
+Colors to_enum(const std::string&);
+std::string to_string(const Colors&);
+
+int main() {
+    std::cout << (int)to_enum("Red");
+    return 0;
+}
+
+Colors to_enum(const std::string& rhs) {
+    std::map<std::string, Colors> map;
+    map["Red"] = Colors::Red;
+    map["Blue"] = Colors::Blue;
+    map["Green"] = Colors::Green;
+    return map[rhs];
+}
+
+std::string to_string(const Colors& rhs) {
+    std::map<Colors, std::string> map;
+    map[Colors::Red] = "Red";
+    map[Colors::Blue] = "Blue";
+    map[Colors::Green] = "Green";
+    return map[rhs];
+}
 
